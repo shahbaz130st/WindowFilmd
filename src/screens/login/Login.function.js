0f9506c40ac8fefdf46da32863d9dadc38c9628c
theherd8@invozone.com <codefic@invozone.com>
2022-04-22 14:39:06 +0500

    latest commit

--- src/screens/login/Login.function.js
+++ src/screens/login/Login.function.js
@@ -8,58 +8,72 @@ import { signIn } from "../../Store/ActionsCreator";
 import firestore from '@react-native-firebase/firestore';
 
 export default LoginFunction = (props) => {
-    const mainApp = StackActions.replace("OnBoarding")
-    const dispatch=useDispatch()
-    const [loading, setLoading] = useState(false)
-    const loginValidation = (email, password) => {
-         if (email === "") {
-            alert("Email is required")
-            // AlertComponent({ msg: "Email/Mobile Number is required", title: "Error", type: "error" })
-        }
-        else if (reg.test(email) == false) {
-            alert("Email is invalid")
-            // AlertComponent({ msg: "Email is invalid", title: "Error", type: "error" })
-        }
-        else if (password == "") {
-            alert("Password is required")
-            // AlertComponent({ msg: "Password is required", type: "error", title: "Error" })
-        }
-        else if (password.length <= 7) {
-            alert("Password must be 8 characters")
-            // AlertComponent({ msg: "Password must be 8 characters", type: "error", title: "Error" })
-        }
-        else {
-            loginApiCall(email, password)
-            // Login_Auth();
-        }
+  const mainApp = StackActions.replace("OnBoarding")
+  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(false)
+  const loginValidation = (email, password) => {
+    if (email === "") {
+      alert("Email is required")
+      // AlertComponent({ msg: "Email/Mobile Number is required", title: "Error", type: "error" })
+    }
+    else if (reg.test(email) == false) {
+      alert("Email is invalid")
+      // AlertComponent({ msg: "Email is invalid", title: "Error", type: "error" })
+    }
+    else if (password == "") {
+      alert("Password is required")
+      // AlertComponent({ msg: "Password is required", type: "error", title: "Error" })
+    }
+    else if (password.length <= 7) {
+      alert("Password must be 8 characters")
+      // AlertComponent({ msg: "Password must be 8 characters", type: "error", title: "Error" })
+    }
+    else {
+      loginApiCall(email, password)
+      // Login_Auth();
+    }
 
+  }
+  const loginApiCall = async (email, password) => {
+    setLoading(true)
+    try {
+      let response = await auth().signInWithEmailAndPassword(email, password)
+      if (response && response.user) {
+        console.log(response.user)
+        Alert.alert("Success ✅", "Authenticated successfully")
+        // firestore()
+        //   .collection('Users')
+        //   .doc("hNcpRolSQGVMRCJ5MqsjQUL9OWq2")
+        //   .get()
+        //   .then(documentSnapshot => {
+        //     console.log('User exists: ', documentSnapshot.exists);
+
+        //     if (documentSnapshot.exists) {
+        //       console.log('User data: ', documentSnapshot.data());
+        //     }
+        //   });
+        // const user = await firestore().collection('Users').doc(response.user.uid).get();
+        // let data = await firestore()
+        //   .collection('Users')
+        //   .doc(response.user.uid)
+        // .onSnapshot(documentSnapshot => {
+        //   console.log('User data: ', documentSnapshot.data());
+        // });
+        console.log(user)
+        setLoading(false)
+        // dispatch(signIn({userData:response?.user,isLogin:'true'}))
+        // props.navigation.dispatch(mainApp)
+      }
+    } catch (error) {
+      if (error.code === 'auth/user-not-found') {
+        Alert.alert("Error", 'Email address is invalid')
+      }
+
+      if (error.code === 'auth/wrong-password') {
+        Alert.alert("Error", 'Password is invalid')
+      }
+      setLoading(false)
     }
-    const loginApiCall = async (email, password) => {
-        setLoading(true)
-        try {
-            let response = await auth().signInWithEmailAndPassword(email, password)
-            if (response && response.user) {
-              Alert.alert("Success ✅", "Authenticated successfully")
-              firestore()
-              .collection('Users')
-              .doc(response.user.uid)
-              .onSnapshot(documentSnapshot => {
-                console.log('User data: ', documentSnapshot.data());
-              });
-              setLoading(false)
-              dispatch(signIn({userData:response?.user,isLogin:'true'}))
-              props.navigation.dispatch(mainApp)
-            }
-          } catch (error) {
-            if (error.code === 'auth/user-not-found') {
-                Alert.alert("Error",'Email address is invalid')
-              }
-          
-              if (error.code === 'auth/wrong-password') {
-                Alert.alert("Error",'Password is invalid')
-              }
-            setLoading(false)
-          }
-    }
-    return [loading, loginValidation];
+  }
+  return [loading, loginValidation];
 };
